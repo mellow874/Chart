b0056aa6347e3ae3ee28d9ad12aa284d8bafe0da
mellow874 <felicitymncube49@gmail.com>
2025-11-02 18:16:11 +0200

    Import components

--- CryptX/src/App.jsx
+++ CryptX/src/App.jsx
@@ -1,1 +1,16 @@
 import React from "react";
+import Sidebar from "./components/Sidebar"; 
+import Header from "./components/Header";
+
+export default function App() {
+    return (
+        <div>
+    {/* Searchbar at the top */}
+      <Header />
+      {/* Different sections */}
+      <main>
+        <Sidebar />
+      </main>
+      </div>
+    );
+}
---
+++ CryptX/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+//import react-icons
+import {FiSearch, FiBell, FiHelpCircle} from "react-icons/fi";
+
+export default function Header() {
+    return (
+        <div></div>
+    );
+}
