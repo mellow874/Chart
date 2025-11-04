3102af0cb50f0944ad266f789a0f1fb6815648f7
mellow874 <felicitymncube49@gmail.com>
2025-11-04 16:19:34 +0200

    Removed the logo syntax and replaced with an image

--- CryptX/src/components/Sidebar.jsx
+++ CryptX/src/components/Sidebar.jsx
@@ -1,22 +1,17 @@
 import React from "react"; 
 //Imported react-icons
-import { TbInfinity } from "react-icons/tb";
 import { FiPieChart, FiRepeat, FiCreditCard, FiMail, FiSettings, FiLogOut} from "react-icons/fi";
 import { AiFillAppstore } from "react-icons/ai";
+import Logo from "../assets/Logo.png";
 
 export default function Navbar() {
     return (
-        <div className="flex h-screen bg-light-gray">
+        <div className="flex h-screen text-bg-light-gray">
 
            {/*Sidebar*/}
            <nav className="px-8 py-6  flex flex-col fixed w-[263px]  h-[1,025px] border-r border-r-[1px]">
             {/*Logo*/}
-            <div className="flex items-center mb-10 gap-[20px]">
-                <TbInfinity className="text-3xl align-middle text-purple-600"/>
-                <h1 className="font-inter font-bold text-[21px] leading-[100%] tracking-[0%] align-middle flex items-center gap-2 text-black">
-                    CryptX
-                </h1>
-            </div>
+            <img src={Logo} className="flex items-center mb-10 gap-[20px]"/>
             {/*Navlist */}
             <ul className="flex flex-col gap-5  mb-4 pb-2 flex items-center text-gray">
                 <li className="text-purple flex items-center gap-[20px] font-semibold"> 
