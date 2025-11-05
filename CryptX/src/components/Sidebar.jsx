79db6e85d3de436eda4e322b3bd03f3efcb605b1
mellow874 <felicitymncube49@gmail.com>
2025-11-05 15:01:45 +0200

    Added margin left

--- CryptX/src/components/Sidebar.jsx
+++ CryptX/src/components/Sidebar.jsx
@@ -14,32 +14,32 @@ export default function Navbar() {
             <img src={Logo} className="flex items-center mb-10 gap-[20px]"/>
             {/*Navlist */}
             <ul className="flex flex-col gap-5  mb-4 pb-2 flex items-center text-gray">
-                <li className="text-purple flex items-center gap-[20px] font-semibold"> 
+                <li className="text-purple text-left flex items-center gap-[20px] font-semibold"> 
                     <AiFillAppstore className="text-xl"/>
                     Overview       
                     <span className="absolute right-10 top-20 text-3xl w-1 h-6 text-purple">.</span>         
                 </li>
-                <li className="flex items-center gap-[20px]"> {/*Ensures that the icon and it's name are inline */}
-                    <FiPieChart/>
+                <li className="flex text-left items-center  gap-[20px]"> {/*Ensures that the icon and it's name are inline */}
+                    <FiPieChart  />
                     Chart
                 </li>
-                <li className="flex items-center gap-[20px]"> 
-                    <FiRepeat/>
+                <li className="flex items-center ml-[50px] gap-[20px]"> 
+                    <FiRepeat />
                     Transactions
                 </li>
-                <li className="flex items-center gap-[20px]">
-                    <FiCreditCard/>
+                <li className="flex items-center ml-[9px] gap-[20px]">
+                    <FiCreditCard />
                     Wallet
                 </li>
-                <li className="flex items-center gap-[20px]">
+                <li className="flex items-center ml-[22px] gap-[20px]">
                     <FiMail/>
                     Mailbox
                 </li>
-                <li className="flex items-center gap-[20px]">
+                <li className="flex items-center ml-[14px] gap-[20px]">
                     <FiSettings/>
                     Setting
                 </li>
-                <li className="flex items-center gap-[20px]">
+                <li className="flex items-center ml-[15px] gap-[20px]">
                     <FiLogOut/>
                     Logout
                 </li>
