37447ff4e977f9567ec7bab0f2eb2a021db6b6e1
mellow874 <felicitymncube49@gmail.com>
2025-11-05 14:41:48 +0200

    Imported Transaction component

--- CryptX/src/App.jsx
+++ CryptX/src/App.jsx
@@ -1,18 +1,21 @@
 import React from "react";
-import Sidebar from "./components/Sidebar"; 
-import Header from "./components/Header";
-import Coins from "./components/Coins"
+import Header from "./components/Header"; 
+import Coins from "./components/Coins";
+import LiveMrkt from "./components/LiveMrkt";
+import Transactions from "./components/Transactions";
 
 export default function App() {
-    return (
+  return (
+    <div className="min-h-screen bg-light-gray flex flex-col">
+      <Header />
+
+      <main className="flex-1 p-8">
         <div>
-    {/* Searchbar at the top */}
-      {/* Different sections */}
-      <main>
-        <Header />
-        <Sidebar />
-        <Coins/>
+        <Coins />
+        <LiveMrkt />
+        <Transactions/>
+        </div>
       </main>
-      </div>
-    );
+    </div>
+  );
 }
