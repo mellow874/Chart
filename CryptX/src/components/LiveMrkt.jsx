ad27dc734ef7ff6447fa2e14969171c44988d940
mellow874 <felicitymncube49@gmail.com>
2025-11-07 14:42:35 +0200

    Update

--- CryptX/src/components/LiveMrkt.jsx
+++ CryptX/src/components/LiveMrkt.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-
-// import images
+//import images
 import BTCLogo from "../assets/BTC logo.png";
 import BTCGraph from "../assets/BTC graph.png";
 import ETHLogo from "../assets/ETH logo.png";
@@ -11,91 +10,81 @@ import ITCLogo from "../assets/ITC logo.png";
 import ITCGraph from "../assets/ITC graph.png";
 
 export default function LiveMarket() {
-  const coins = [
-    {
-      logo: ETHLogo,
-      graph: ETHGraph,
-      name: "Ethereum",
-      symbol: "ETH / USDT",
-      change: "+14.02%",
-      price: "39,786 USD",
-      changeColor: "text-green-500",
-    },
-    {
-      logo: BTCLogo,
-      graph: BTCGraph,
-      name: "Bitcoin",
-      symbol: "BTC / USDT",
-      change: "+4.02%",
-      price: "21,786 USD",
-      changeColor: "text-green-500",
-    },
-    {
-      logo: ITCLogo,
-      graph: ITCGraph,
-      name: "Litecoin",
-      symbol: "LTC / USDT",
-      change: "-9.02%",
-      price: "9,786 USD",
-      changeColor: "text-red-500",
-    },
-    {
-      logo: ADALogo,
-      graph: ADAGraph,
-      name: "Cardano",
-      symbol: "ADA / USDT",
-      change: "+4.86%",
-      price: "4,786 USD",
-      changeColor: "text-green-500",
-    },
-  ];
+    //Array of objects which include the logo, image, name, price, symbol...
+    const coins = [
+        {
+            logo: ETHLogo,
+            graph: ETHGraph,
+            name: "Ethereum",
+            symbol: "ETH / USDT",
+            change: "+14.02%",
+            price: "39,786 USD",
+        },
+        {
+            logo: BTCLogo,
+            graph: BTCGraph,
+            name: "Bitcoin",
+            symbol: "BTC / USDT",
+            change: "+4.02%",
+            price: "21,786 USD",
+        },
+        {
+            logo: ITCLogo,
+            graph: ITCGraph,
+            name: "Litecoin",
+            symbol: "LTC / USDT",
+            change: "-9.02%",
+            price: "9,786 USD",
+        },
+        {
+            logo: ADALogo,
+            graph: ADAGraph,
+            name: "Cardano",
+            symbol: "ADA / USDT",
+            change: "+4.86%",
+            price: "4,786 USD",
+        },
+    ];
 
-  return (
-    <section className="mt-10">
-      <h2 className="text-2xl font-bold text-gray-800 mb-4">Live Market</h2>
+    return (
+        <section>
+            {/*Heading of the section*/}
+            <h2 className="text-2xl font-semibold text-heading mb-4">Live Market</h2>
 
-      <div>
-        {coins.map((coin, index) => (
-          <div
-            key={index}
-            className="grid grid-cols-5 gap-x-4 items-center py-2 text-sm"
-          >
-            {/* Coin logo */}
-            <img
-              src={coin.logo}
-              alt={coin.name}
-              className="w-12 h-12 object-contain"
-            />
+            <div className="space-y-4">
+                {/*Map of the array, showing how the objects should align */}
+                {coins.map((coin, index) => (
+                    <div key={index} className="grid grid-cols-[auto_1fr_auto_auto_1fr] gap-x-2 items-center">
+                        
+                        {/*Logo images for the livemarket*/}
+                        <img src={coin.logo} alt={coin.name} className="w-[50px] h-[50px] object-contain"/>
 
-            {/* Coin name + symbol */}
-            <div>
-              <p className="font-semibold text-gray-800">{coin.name}</p>
-              <p className="text-xs text-gray-500">{coin.symbol}</p>
+                        <div>
+                            <p className="font-semibold text-heading">{coin.name}</p>
+                            <p className="text-xs text-coin">{coin.symbol}</p>
+                        </div>
+
+                        <div>
+                            <p className="text-xs text-coin market">Change</p>
+                            <p className={`font-semibold ${coin.change}`}>
+                                {coin.change}
+                            </p>
+                        </div>
+
+                    
+                        <div className="ml-10">
+                            <p className="text-x text-gray-500">Price</p>
+                            <p className="font-semibold text-gray-800">{coin.price}</p>
+                        </div>
+
+                        <img
+                            src={coin.graph}
+                            alt={`${coin.name} chart`}
+                            className="w-full h-8 object-contain ml-4"
+                        />
+                    </div>
+                ))}
             </div>
-
-            {/* Change */}
-            <div>
-              <p className="text-xs text-gray-500">Change</p>
-              <p className={`font-semibold ${coin.changeColor}`}>
-                {coin.change}
-              </p>
-            </div>
-
-            {/* Price */}
-            <div>
-              <p className="text-xs text-gray-500">Price</p>
-              <p className="font-semibold text-gray-800">{coin.price}</p>
-            </div>
-
-            {/* Chart image */}
-            <img
-              src={coin.graph}
-              alt={`${coin.name} chart`}
-              className="w-full h-6 object-contain"
-            />
-          </div>
-        ))}
-      </div>
-    </section>
-  );
+        </section>
+    );
 }
---
+++ CryptX/src/components/Transactions.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+
+import ETH from "../assets/ETH Trans.png";
+import BTC from "../assets/BTC Trans.png";
+import BTC2 from "../assets/2BTC Trans.png";
+import ITC from "../assets/ITC Trans.png";
+
+export default function Transactions() {
+    const coins = [
+        {
+            logo: ETH,
+            name: "Ethereum",
+            transaction: "Received",
+            price: "24,102 USD",
+            date: "Today, 19:30",
+        },
+        {
+            logo: BTC,
+            name: "Bitcoin",
+            transaction: "Buy",
+            price: "4,1576 USD",
+            date: "Today, 14:32",
+        },
+        {
+            logo: BTC2,
+            name: "Bitcoin",
+            transaction: "Buy",
+            price: "64,784 USD",
+            date: "Today, 13:50",
+        },
+        {
+            logo: ITC,
+            name: "Litecoin",
+            transaction: "Buy",
+            price: "14,265 USD",
+            date: "Today, 09:38",
+        },
+    ];
+
+    return (
+        //Ensures that the transaction sectionn is on the left side
+        <section className="ml-30">
+            <h2 className="text-2xl font-semibold text-heading mb-4">Transactions</h2>
+
+            <div className="space-y-4">
+                {coins.map((coin, index) => (
+                    //Everything stays in a grid format
+                    <div key={index} className="grid grid-cols-[auto_1fr_auto_auto_1fr] items-center gap-x-2">
+
+                        <img
+                            src={coin.logo}
+                            alt={coin.name}
+                            className="w-[50px] h-[50px] object-contain space-x-1"
+                        />
+        
+                    <div className="col-span-2 flex flex-col">
+                            <p className="font-semibold space-x-2 text-gray-800">{coin.name}</p>
+                            <p className="text-xs text-gray-500">{coin.transaction}</p>
+                        </div>
+
+                        <div className="ml-40">
+                            <p className="font-semibold text-gray-800">${coin.price.split(' ')[0]}</p>
+                            <p className="text-xs text-gray-500">{coin.date}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </section>
+    );
+}
